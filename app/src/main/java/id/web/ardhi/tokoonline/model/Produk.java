8e2effe5b9988238a15dc990befc8b6b63cf40c6
agung79 <54230038+agung79@users.noreply.github.com>
2025-04-29 13:15:42 +0700

    Add files via upload

---
+++ app/src/main/java/id/web/ardhi/tokoonline/model/Produk.java
@@ -0,0 +1,73 @@
+
+package id.web.ardhi.tokoonline.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Produk {
+
+    @SerializedName("id_produk")
+    @Expose
+    private Integer idProduk;
+    @SerializedName("nama_produk")
+    @Expose
+    private String namaProduk;
+    @SerializedName("id_kategori")
+    @Expose
+    private Integer idKategori;
+    @SerializedName("harga")
+    @Expose
+    private Integer harga;
+    @SerializedName("deskripsi")
+    @Expose
+    private String deskripsi;
+    @SerializedName("gambar")
+    @Expose
+    private String gambar;
+    @SerializedName("created_at")
+    @Expose
+    private String createdAt;
+    @SerializedName("updated_at")
+    @Expose
+    private String updatedAt;
+    @SerializedName("kategori")
+    @Expose
+    private Kategori kategori;
+
+    public Kategori getKategori(){
+        return kategori;
+    }
+
+    public Integer getIdProduk() {
+        return idProduk;
+    }
+
+
+    public String getNamaProduk() {
+        return namaProduk;
+    }
+
+    public Integer getIdKategori() {
+        return idKategori;
+    }
+
+    public Integer getHarga() {
+        return harga;
+    }
+
+    public String getDeskripsi() {
+        return deskripsi;
+    }
+
+    public String getGambar() {
+        return gambar;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+}
